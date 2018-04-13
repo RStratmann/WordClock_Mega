796520aa83468acda998288a6969cf9478ea9977
Rüdiger Stratmann <ruediger.stratmann@web.de>
2018-04-14 01:20:47 +0200

    NEOPIXEL- und ST7735-Library für TFT-Display parallel eingebunden

--- WordClock_Mega.ino
+++ WordClock_Mega.ino
@@ -1,16 +1,34 @@
-/*
+// Includes für TFT-Anzeige
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+#include <Fonts/FreeSansOblique18pt7b.h>
+#include <Fonts/FreeSansOblique12pt7b.h>
 
- */
+// Include für Neopixel-LED-Stripes
 #include "FastLED.h"
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define NUM_LEDS 6
-#define DATA_PIN 6
+// Include für Schnittstellen
+#include <SPI.h>
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+
+// Defines zur Pin-Zuordnung für 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    9
+#define TFT_DC     8
+
+// Defines für Neopixel-LED-Stripe
+#define NUM_LEDS 6        // Anzahl der verwendeten LEDs
+#define DATA_PIN 6        // Data-Pin des LED-Stripes liegt auf Arduino-Pin 6
+
+
+
+// Initialisierung LED-Stripe
+CRGB leds[NUM_LEDS];      // Initialisierung  LED-Stripe
+
+// Initialisierung TFT-Display
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
 
 
 
@@ -18,13 +36,36 @@ void setup() {
 
       // Die WordClock_Mega benutzt LED-Stripes aus der NeoPixel-Serie
       FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
+        /* die serielle Ausgabe initialisieren */
+      Serial.begin(115200);
+      Serial.println("Hallo");
+
+      // Use this initializer if you're using a 1.8" TFT
+      tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+
+      tft.setTextWrap(false);
+      tft.setRotation(3);
+
+      Serial.println("Initialized");
 }
+
+
 
 void loop() {
 
-//  leds[1] = CRGB::Red;
-//  leds[2] = CRGB::Red;
-//  leds[3] = CRGB::Red;
+  char clockString[30];
+
+
+
+  // put your main code here, to run repeatedly:
+  tft.fillScreen(ST7735_BLUE);
+
+  tft.setFont(&FreeSansOblique18pt7b);
+  tft.setTextColor(ST7735_YELLOW);
+
+  tft.setCursor(10, 40);
+//  tft.setTextSize(2);
 
     for (int j=0; j<7; j++)
     {
@@ -40,32 +81,50 @@ void loop() {
                 switch (j) {
                   case 0 :
                     leds[i] = CRGB::Yellow;
+                    sprintf(clockString, "Gelb");
                   break;
                   case 1 :
                     leds[i] = CRGB::Aqua;
+                    sprintf(clockString, "Aqua");
                   break;
                   case 2 :
                     leds[i] = CRGB::Red;
+                    sprintf(clockString, "Rot");
                   break;
                   case 3 :
                     leds[i] = CRGB::Purple;
+                    sprintf(clockString, "Lila");
                   break;
                   case 4 :
                     leds[i] = CRGB::Green;
+                    sprintf(clockString, "Grün");
                   break;
                   case 5 :
                     leds[i] = CRGB::Blue;
+                    sprintf(clockString, "Blau");
                   break;
                   default :
                     leds[i] = CRGB::White;
+                    sprintf(clockString, "Weiß");
 
-                }
+               }
             }
 
             FastLED.show();
 
-            delay(100);
+            tft.setCursor(10, 40);
+             //  tft.setTextSize(2);
+
+            tft.println(clockString);
+
+            delay(500);
         }
+
+        tft.setTextColor(ST7735_BLUE);
+        tft.setCursor(10, 40);
+        tft.println(clockString);
+        tft.setTextColor(ST7735_YELLOW);
+
     }
 
 
@@ -80,31 +139,48 @@ void loop() {
 
       switch (l)
       {
-        case 0 :
           leds[k] = CRGB::Yellow;
+          sprintf(clockString, "Gelb");
         break;
         case 1 :
           leds[k] = CRGB::Aqua;
+          sprintf(clockString, "Aqua");
         break;
         case 2 :
           leds[k] = CRGB::Red;
+          sprintf(clockString, "Rot");
         break;
         case 3 :
           leds[k] = CRGB::Purple;
+          sprintf(clockString, "Lila");
         break;
         case 4 :
           leds[k] = CRGB::Green;
+          sprintf(clockString, "Grün");
         break;
         case 5 :
           leds[k] = CRGB::Blue;
+          sprintf(clockString, "Blau");
         break;
         default :
           leds[k] = CRGB::White;
+          sprintf(clockString, "Weiß");
 
       }
       FastLED.show();
 
-      delay(500);
+      tft.setCursor(10, 40);
+
+      tft.println(clockString);
+
+
+      delay(2500);
+
+      tft.setTextColor(ST7735_BLUE);
+      tft.setCursor(10, 40);
+      tft.println(clockString);
+      tft.setTextColor(ST7735_YELLOW);
     }
+
 }
 
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,10 +2,16 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-04-14 00:40:55
+//This file has been generated on 2018-04-14 01:19:30
 
 #include "Arduino.h"
+#include <Adafruit_ST7735.h>
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+#include <Fonts/FreeSansOblique18pt7b.h>
+#include <Fonts/FreeSansOblique12pt7b.h>
 #include "FastLED.h"
+#include <SPI.h>
 
 void setup() ;
 void loop() ;
